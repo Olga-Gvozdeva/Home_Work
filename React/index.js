149c935f50c585f5c82154463a98da26229ff57f
Olga Gvozdeva <atlasink@mail.ru>
2022-06-30 13:30:21 +0300

    done

---
+++ React/index.js
@@ -0,0 +1,35 @@
+/*
+let beat = function(){
+    let haha = document.createElement('div');
+    haha.className = 'pisa';
+    haha.innerHTML = '<p>Piiiiiisaaaa kiiiiisaaaaa</p>';
+    document.body.append(haha);
+}
+
+document.addEventListener("DOMContentLoaded", beat);
+
+document.addEventListener("DOMContentLoaded", function(){
+    let haha = document.createElement('div');
+    haha.className = 'pisa';
+    haha.innerHTML = '<p>Piiiiiisaaaa kiiiiisaaaaa</p>';
+    document.body.append(haha);
+})
+
+
+const e = React.createElement;
+
+return(
+    <button onClick={() => this.setState({liked: true})}>
+    Нравится
+    </button>
+);
+*/
+
+
+function app(){
+    return <div>
+        <p>Privet</p>
+    </div>
+}
+
+app();
